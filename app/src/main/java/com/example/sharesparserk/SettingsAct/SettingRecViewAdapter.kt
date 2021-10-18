fc6b9efcf55e90eef7fa8c4a31142369eadd6497
Rulevsky <ruleaa@gmail.com>
2021-10-18 18:42:24 +0300

    working app

---
+++ app/src/main/java/com/example/sharesparserk/SettingsAct/SettingRecViewAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.sharesparserk.SettingsAct
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sharesparserk.R
+import com.example.sharesparserk.database.SettingsForStocks
+
+class SettingRecViewAdapter(private var settingsDatabaseDao: MutableList<SettingsForStocks>):
+    RecyclerView.Adapter<SettingRecViewAdapter.ViewHolder>(){
+
+
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idSetTextView: TextView
+        val lowPriceSetTextView: TextView
+        val highPriceSetTextView: TextView
+        init{
+            idSetTextView = view.findViewById(R.id.idSetTextView)
+            lowPriceSetTextView = view.findViewById(R.id.lowPriceSetTextView)
+            highPriceSetTextView = view.findViewById(R.id.highPriceSetTextView)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.settings_holder, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.idSetTextView.text = settingsDatabaseDao.get(position)?.settingsID.toString()
+        holder.lowPriceSetTextView.text = settingsDatabaseDao.get(position)?.lowPrice.toString()
+        holder.highPriceSetTextView.text = settingsDatabaseDao.get(position)?.highPrice.toString()
+
+    }
+
+    override fun getItemCount() = 32
+}
